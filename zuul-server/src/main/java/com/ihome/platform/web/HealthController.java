5b98e607f9f71092e66e2ab032f78c05eccea5fc
panjin-sz <panjin2015@gmail.com>
2018-07-22 22:40:56 +0800

    add zuul server

--- zuul-server/src/main/java/com/ihome/platform/web/HealthController.java
+++ zuul-server/src/main/java/com/ihome/platform/web/HealthController.java
@@ -1,7 +1,6 @@
-package com.ihome.basicbiz.web;
+package com.ihome.platform.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,18 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author panjin.
  * @date 2018/7/22.
  */
-@RestController
 @Slf4j
+@RestController
 public class HealthController {
-
-    @Value("${from}")
-    private String from;
 
     @GetMapping(value = "/health")
     public String health() {
         log.info("--------------------info health-------------------");
 
-        log.info("from is {}", from);
-        return "hello, customer!";
+        return "hello, zuul!";
     }
 }
